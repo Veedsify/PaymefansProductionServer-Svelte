40e60f31fec2387b9b1d48e42761d2a36e13b5c9
= <=>
2025-05-14 20:23:06 +0100

    refactor: replace fetch with axios for user data retrieval and improve error handling

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,26 +1,34 @@
 import type { LayoutServerLoad } from './$types';
 import type { AuthUserProps } from '../../types/user';
 import { PUBLIC_TS_EXPRESS_URL } from '$env/static/public';
+import { goto } from '$app/navigation';
+import { redirect } from '@sveltejs/kit';
+import axios from 'axios';
 export const load: LayoutServerLoad = async ({ cookies }) => {
     async function retrieveUserData() {
         const token = cookies.get('token');
         try {
-            const response = await fetch(`${PUBLIC_TS_EXPRESS_URL}/auth/retrieve`, {
-                method: 'GET',
-                cache: "default",
+            const response = await axios.get(`${PUBLIC_TS_EXPRESS_URL}/auth/retrieve`, {
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${token}`
                 }
             });
-            if (response.ok) {
-                const data = await response.json();
-                if (data?.user) {
-                    return data.user as AuthUserProps;
-                }
+            if (response.status !== 200) {
+                return redirect(200, '/login');
             }
-        } catch (error) {
-            console.error('Error retrieving user data:', error);
+
+            const data = response.data;
+            if (data?.user) {
+                return data.user as AuthUserProps;
+            }
+
+        } catch (error: any) {
+            cookies.delete('token', {
+                expires: new Date(0),
+                path: '/',
+            });
+            redirect(307, '/login');
         }
     }
     const user = await retrieveUserData();
